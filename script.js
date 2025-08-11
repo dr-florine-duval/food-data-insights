ed5ff15e89439cc4682f0126c1a80a2084a540ac
dr-florine-duval <134215669+dr-florine-duval@users.noreply.github.com>
2025-08-11 17:46:55 +0200

    update styles.css and script.js

--- script.js
+++ script.js
@@ -13,7 +13,10 @@ const tooltipContent = `
     <strong>Extra tip:</strong> click and drag from right to left to zoom horizontally only.<br>
     📷 Enjoy the interactive chart, and feel free to download the plot as a png as a souvenir!<br>
     <br>
-    <strong><i>Need more space? Click on this ⛶ icon on the left of the chart to open it in its own new tab.</i></strong><br>
+    <strong><i>Need more space? Click on this <span class=open-new-plot-icon-in-js><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+</svg></span>
+ icon on the left of the chart to open it in its own new tab.</i></strong><br>
 `;
 
 const helpButtons = document.querySelectorAll('.help-button');
